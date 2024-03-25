ddda247ce6ba6cc3ffeb29e6e915f14b086c8b63
Tonny Mulli <tonnymulli2118@gmail.com>
2024-03-25 21:43:58 +0300

    Fourth JS

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random())+1;
+
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+let numbers = [5, 12, 20, 8, 3];
+let max = Math.max(...numbers);
+
+
 // Write your code here
